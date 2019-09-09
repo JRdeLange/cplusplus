d56731bbe69f677a30bfb64a6f1de03a9b5a8a50
Jordy de Lange <mrshroom1988@Gmail.com>
2019-09-09 13:57:39 +0200

    message

---
+++ week1/exercise4.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    std::cout << "^\\s+Encryption key:(\\w+)"        << '\n' << 
+                 "^\\s+Quality=(\\d+)"               << '\n' <<
+                 "^\\s+E?SSID:\"([[:print:]]+)\""    << '\n' <<
+                 "^\\s+ssid=\"([[:print:]]+)\""      << '\n';
+}
---
+++ week1/hello.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello World\n";					//insert Hello World into the output stream
+}
