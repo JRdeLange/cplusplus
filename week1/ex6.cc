a6e5512863469fb284fbce170df730d027a8c948
Job <jobheeres@hotmail.nl>
2019-09-11 22:15:13 +0200

    added ex6

---
+++ week1/ex6.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int value;
+	cin >> value;
+
+	cout << (value % 2 ? "odd" : "even") << '\n';
+	/*Dividing an odd number by 2 results in a modulus of 1*/
+
+	cout << (value / 2 * 2 == value ? "even" : "odd") << '\n';
+	/*Becouse integers have to be whole numbers, an odd number divided by 2 will be rounded. This means
+	that */			
+
+	cout << (value & 1 ? "odd" : "even") << '\n';				
+	/*By using the Bitwise AND over value and 1, we are checking whether value in binairy starts with a 1.
+	 If so then value has to be odd*/
+
+	cout << ((value >> 1) << 1 == value ? "even" : "odd") << '\n';		
+	/*By first shifting the bits to the right and then to the left, the last bit will be changed to a zero.
+	If we then compare this new value to the input we can determine whether the input is odd or even.*/
+
+	cout << ((value | 1) - value ? "even" : "odd") << '\n';
+
+	cout << (value ? "odd" : "even") << '\n';
+}
