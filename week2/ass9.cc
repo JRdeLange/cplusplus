18473e46dfdb66aa7b4715fb90a72325b9582cc5
Job <jobheeres@hotmail.nl>
2019-09-13 12:11:40 +0200

    spaties vervangen voor tabs

--- week2/ass9.cc
+++ week2/ass9.cc
@@ -4,11 +4,11 @@ using namespace std;
 
 int main(int argc, char *argv[]) 
 {
-    int base;										// The integer
-    base = stoi(argv[1]);							// into which the base is stored
+	int base;										// The integer
+	base = stoi(argv[1]);							// into which the base is stored
 
-    for (int mltply = 1; mltply <= 10; ++mltply)	// Multiply from 1 to 10
+	for (int mltply = 1; mltply <= 10; ++mltply)	// Multiply from 1 to 10
 
-    	cout << mltply << " * " << base 			// Show the calculation
-    		 << " = " << mltply*base << "\n";		// and the answer
+		cout << mltply << " * " << base 			// Show the calculation
+			 << " = " << mltply*base << "\n";		// and the answer
 }			
