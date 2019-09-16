f2e2ad4b9e2b4cb58ee39c3afe6eff3b041bece1
Jordy de Lange <mrshroom1988@Gmail.com>
2019-09-16 13:50:38 +0200

    ass 12 werkt, nog niet gecomment

--- week2/ass12.cc
+++ week2/ass12.cc
@@ -10,8 +10,27 @@ int main()
 	while (true)						// Keep reading the next line
 	{
 		getline(cin, currLine);			// Get the current line
-										// Check if there is more than one word
-		
-		for (size_t idx = 0; idx < currLine.length(); ++idx)
+		size_t firstSpace;				// Pos of first space is stored here
+		firstSpace = currLine.find(' ');
+
+		if (firstSpace != string::npos)	// If there is no space we dont need
+		{								// to switch words
+			size_t lastSpace;
+			lastSpace = currLine.find_last_of(' ');
+
+			if (lastSpace != string::npos)	
+			{
+				size_t lastWordLength;	//The length of the last word
+				lastWordLength = currLine.length() - lastSpace;
+				string lastWord;
+				//lastWord.copy(currLine, lastWordLength, lastSpace);
+
+
+
+				currLine.replace(0, firstSpace, currLine, lastSpace+1, lastWordLength);
+			}
+
+		}
+	cout << currLine << "\n";
 	}
 }
