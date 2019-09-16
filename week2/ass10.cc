ddb3cfeec49c1761412fb5f5e46bee4ff57dce63
tux <mrshroom1988@gmail.com>
2019-09-16 10:21:51 +0200

    numero 12 begonnen

--- week2/ass10.cc
+++ week2/ass10.cc
@@ -8,7 +8,7 @@ int main()
 	const int highestAscii = 255;		// Since ascii goes from 0 to 255
 	string allLetters;					// This will be the output
 										// Loop through all possible chars
-	for (int symbol = 0; symbol < highestAscii; ++symbol)
+	for (size_t symbol = 0; symbol < highestAscii; ++symbol)
 		if (isalpha(symbol))			// If symbol is a letter
 			allLetters += symbol;		// add it to the string
 	
---
+++ week2/ass12.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ctype.h>
+#include <string>
+using namespace std;
+
+int main() 
+{
+	string currLine;					// The string that will be worked on
+
+	while (true)						// Keep reading the next line
+	{
+		getline(cin, currLine);			// Get the current line
+										// Check if there is more than one word
+		
+		for (size_t idx = 0; idx < currLine.length(); ++idx)
+	}
+}
